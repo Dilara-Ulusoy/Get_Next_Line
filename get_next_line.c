ac26791944a57624561b471baf6a174d88424d87
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-05-13 13:25:09 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -9,49 +9,72 @@ char *get_next_line(int fd)
     line = NULL; 
     buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
     if (!buffer)
-        return (NULL);
-    if (valide_fd(fd, buffer, stash) == -1)
-        return (NULL);
-    stash = get_stash_from_buffer(fd, stash, buffer);
-    if (*stash == 0) // If the first character of stash is NULL or empty string we free the stash and return NULL
     {
         free(stash);
         return (stash = 0);
     }
-    line = extract_line(stash, line); // We extract the first line from the stash
-    stash = extract_remaining_stash(stash); // We extract the remaining stash
-    return (line);
-}
-// Check if the file descriptor is valid or buffer size is valid or read function is valid
-int valide_fd(int fd, char *buffer, char *stash) 
-{
     if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0) 
     {
         free(buffer); 
         free(stash); 
         stash = NULL;
-        return (-1);
+        return (NULL);
     }
-    return (0);
+       
+    stash = get_stash_from_buffer(fd, stash, buffer);
+    if (!stash || *stash == 0) // If the first character of stash is NULL or empty string we free the stash and return NULL
+    {
+        free(stash);
+        return (stash = 0);
+    }
+    line = extract_line(stash, line); // We extract the first line from the stash
+    if (!line)
+    {
+        free(stash);
+        return (stash = 0); // If line is NULL we return NULL
+    }
+    stash = extract_remaining_stash(stash); // We extract the remaining stash
+    if (!stash)
+    {
+        free(line);
+        free(stash);
+        return (stash = 0);
+    }
+    return (line);
 }
-
 char *get_stash_from_buffer(int fd, char *stash, char *buffer)
 {
     int nbytes;
+    char    *temp;
 
     nbytes = 1;
     if (!stash) // If stash is NULL
-        stash = ft_strdup(""); // We allocate memory for stash and copy the empty string to it.
+    {
+        stash = ft_strdup("");
+        if (!stash) // We allocate memory for stash and copy the empty string to it.
+        {
+            free(buffer);
+            return (NULL);
+        }
+    }
     while (nbytes > 0)
     {
         nbytes = read(fd, buffer, BUFFER_SIZE);
         if (nbytes == -1)
         {
+            free(stash);
             free(buffer);
             return (NULL);
         }
         buffer[nbytes] = '\0';
-        stash = ft_strjoin(stash, buffer);
+        temp = ft_strjoin(stash, buffer);
+        if (!temp)
+        {
+            free(stash);
+            free(buffer);
+            return (NULL);
+        }
+        stash = temp;
         if (ft_strchr(buffer, '\n'))
             break;
     }
@@ -92,7 +115,10 @@ char *extract_remaining_stash(char *stash)
     len = find_newline_index(stash);
     remaining_stash = (char *)malloc((ft_strlen(stash) - len + 1) * sizeof(char));
     if (!remaining_stash)
-        return NULL;
+    {
+        free(stash);
+        return (stash = 0);
+    }
     while (stash[len + i] != '\0')
 	{
 		remaining_stash[i] = stash[len + i];
@@ -113,14 +139,3 @@ int find_newline_index(char *stash)
         i++;
     return (i);
 }
-
-
-
-
-
-//Fonksiyonu her cagirdigimizda surekli buffer size kadar okuma yapacak ve okudugunu buffer'a atacak 
-//Fonksiyonu ilk defa cagirdigimizda newline karakteri bulana kadar while dongusu ile buffer size kadar okuma yapacak ve okudugunu stash'a atacak (strjoin ile)
-//Buffer da newline gordugunde dönduguden cikacak
-//Sonrasinda newline'a kadar olan kismi alip line'a atacak ve line'i return edecek
-//Newline'dan sonrasi kismi extract edip stash'a atacak
-//Fonksiyon bir daha cagirildiginda yine ayni islemler gercekqlestirilecek ve stash zaten newline'dan sonrasi olacak.
